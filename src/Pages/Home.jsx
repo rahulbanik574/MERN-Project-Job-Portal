d18af7801f628b63b0a5660649d4678f3925396e
rahulbanik574 <rahulbanik574@gmial.com>
2024-01-02 12:18:22 +0530

    Updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Banner from '../components/Banner'
 
 const Home = () => {
 
+const [selectedCatagory, setSelectedCatagory] = useState(null);
+const[jobs, setJobs]= useState([]);
+
+useEffect(() => {
+fetch("jobs.json").then(res => res.json()).then(data => {
+  // console.log(data)
+  setJobs(data)
+})
+}, [])
+//console.log(jobs)
+
+
+//handle input change 
   const [query, setQuery] = useState("");
   const handleInputChange = (event) => {
     setQuery(event.target.value)
   }
-  console.log(query)
+
+  //filter jobs by title
+  const filteredItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  console.log(filteredItems)
 
   return (
     <div>
