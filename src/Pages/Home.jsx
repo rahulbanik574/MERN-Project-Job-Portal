5cd0e50bc5c9dacbe0e89bf3ebde886a2a92bd47
rahulbanik574 <rahulbanik574@gmial.com>
2024-01-03 11:15:10 +0530

    Updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,19 +3,21 @@ import Banner from '../components/Banner'
 
 const Home = () => {
 
-const [selectedCatagory, setSelectedCatagory] = useState(null);
-const[jobs, setJobs]= useState([]);
+  const [selectedCatagory, setSelectedCatagory] = useState(null);
+  const [jobs, setJobs] = useState([]);
 
-useEffect(() => {
-fetch("jobs.json").then(res => res.json()).then(data => {
-  // console.log(data)
-  setJobs(data)
-})
-}, [])
-//console.log(jobs)
+  useEffect(() => {
+    fetch("jobs.json").then(res => res.json()).then(data => {
+      // console.log(data)
+      setJobs(data)
+    })
+  }, [])
+  //console.log(jobs)
 
 
-//handle input change 
+
+
+  //handle input change 
   const [query, setQuery] = useState("");
   const handleInputChange = (event) => {
     setQuery(event.target.value)
@@ -23,11 +25,47 @@ fetch("jobs.json").then(res => res.json()).then(data => {
 
   //filter jobs by title
   const filteredItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-  console.log(filteredItems)
+  //console.log(filteredItems)
+
+
+  //......Radio Filtering......
+  const handleChange = (event) => {
+    setSelectedCatagory(event.target.value)
+  }
+
+
+  //.....button based filtering.....
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value)
+  }
+
+
+  // main function
+  const filtereData = (jobs, selected, query) => {
+    let filteredJobs = jobs;
+
+    //filtering input items
+    if (query) {
+      filteredJobs = filteredItems;
+    }
+
+    //category filtering
+    if (selected) {
+      filteredJobs = filteredJobs.filter(({ jobLocation, maxPrice, experienceLevel, salaryType, empLoymentType, postingDate }) => (
+jobLocation.toLowerCase() === selected.toLowerCase() ||
+parseInt(maxPrice) <= parseInt(selected) ||
+salaryType.toLowerCase() === selected.toLowerCase() ||
+empLoymentType.toLowerCase() === selected.toLowerCase()
+      ))
+    }
+
+
+  }
+
 
   return (
     <div>
-      <Banner  query={query} handleInputChange={handleInputChange}/>
+      <Banner query={query} handleInputChange={handleInputChange} />
     </div>
   )
 }
